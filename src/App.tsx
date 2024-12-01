ba5abcf27764814eab6df801269f83bd44b8ed6c
sauce-coder-2025 <luke.gray@fisherpaykel.com>
2024-12-02 09:57:10 +1300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ const App: React.FC = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
           <div className="flex items-center">
             <img
-              src="/fp-logo.png"
+              src="./fp-logo.png"
               alt="Fisher & Paykel"
               className="h-8 w-auto"
             />
