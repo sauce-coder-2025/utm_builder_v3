aaa9b326e4c2b849b04a45f23739d77d46a509e9
sauce-coder-2025 <luke.gray@fisherpaykel.com>
2024-12-02 10:30:14 +1300

    Delete docs/assets/vendor.js

--- docs/assets/vendor.js
+++
@@ -1,6 +0,0 @@
-// Basic React imports with modern ES modules
-import React from 'https://esm.sh/react@18.2.0';
-import ReactDOM from 'https://esm.sh/react-dom@18.2.0';
-
-export const r = React;
-export const R = ReactDOM;
